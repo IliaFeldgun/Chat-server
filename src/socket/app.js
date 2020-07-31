4528b19def807aee890aff8b642bda15f6d6b1e8
Ilia Feldgun <iliafeldgun@outlook.com>
2020-07-31 18:42:08 +0300

    Messages go to database

--- src/socket/app.js
+++ src/socket/app.js
@@ -1,34 +1,55 @@
 import SocketIO from 'socket.io'
-
-const users = []
-
+import {MessageModel} from '../db/sequelize'
+let users = []
 const app = (socketServer) => {
     socketServer.on('connection', (socket) => {
         sendUsers()
-
+        sendLastMessages(socket, 10)
         socket.on('message', (message) => {
             if (socket.userName) {
-                socketServer.emit('message', {userName: socket.userName, message})
+                message.userName = socket.userName
+                MessageModel.create(message)
+
+                socketServer.emit('message', message)
             }
         })
 
         socket.on('disconnect', () => {
-
-            users.filter((user) => {
+            
+            users = users.filter((user) => {
                 return socket.userName !== user
             })
         })
 
         socket.on('login', (userName) => {
-            socket.userName = userName
+            if (socket.userName !== userName) {
+                socket.userName = userName
+                users.push(userName)
+            }
 
-            users.push(userName)
+            socket.emit('logged-in', {
+                userName
+            })
+
             sendUsers()
         })
     })
 
     const sendUsers = () => {
         socketServer.sockets.emit('users', users)
+    }
+
+    const sendLastMessages = (socket, limit) => {
+        const lastMessages = MessageModel.findAll({
+            limit,
+            order: [
+                ['timestamp', 'DESC']
+            ]
+        })
+
+        lastMessages.then((messages) => {
+            socket.emit('bulk-message', message)
+        })
     }
 }
 export default app
