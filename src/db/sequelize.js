d43e6f6357f417fde36f4277d980afce620abbd8
Ilia Feldgun <iliafeldgun@outlook.com>
2020-07-31 18:42:30 +0300

    Added model and sequelize init

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,5 +1,23 @@
 import Sequelize from 'sequelize'
 
-const sequelize = new Sequelize(process.env.MYSQL_CONNECTION_STRING)
+const sequelize = new Sequelize(process.env.MYSQL_CONNECTION_STRING, {
+  dialect: 'mysql'
+})
+sequelize.authenticate().then((res) => {
+    console.log(`${res} connected`)
+}).catch ((err) => {
+    console.log(err)
+})
+const MessageModel = sequelize.define('Message', {
+    userName: Sequelize.STRING,
+    message: Sequelize.TEXT,
+    timestamp: Sequelize.DATE
+})
+// sequelize.sync({force: true, alter: true}).then((res) => {
+sequelize.sync().then((res) => {
+    console.log(`synced ${res}`)
+}).catch((res) => {
+    console.log(`sync failed ${res}`)
+})
 
-export default sequelize
+export {MessageModel}
